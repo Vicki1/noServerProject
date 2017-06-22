1c8dcb2ada4d1bbd3508c545ecf768660a2b110c
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2017-06-22 17:00:33 -0600

    in progress

--- src/components/PatientInfo/PatientInfo.js
+++ src/components/PatientInfo/PatientInfo.js
@@ -1,12 +1,21 @@
 import React,{Component} from 'react';
 
-
+var calculatedBMI = 24;
 
 export default class PatientInfo extends Component{
     render(){
         return(
             <div>
-                This is the patient info
+                <h3>What is your BMI?</h3>
+                 I am female
+                 <br/>
+                 <br/>
+              I weigh <input/>lbs.
+               <br/>
+               <br/>
+               I am <input/> inches tall
+               <br/>
+               <h4>Your BMI is : {calculatedBMI}</h4>
             </div>
         )
     }
