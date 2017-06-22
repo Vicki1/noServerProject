ca050cbe598ebceb5f4137973ca637a971c16381
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2017-06-22 17:44:11 -0600

    in progress

--- src/components/PatientInfo/PatientInfo.js
+++ src/components/PatientInfo/PatientInfo.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react';
 
-var calculatedBMI = 24;
+
 
 export default class PatientInfo extends Component{
     constructor(){
         super();
         this.state={
             startingWeight: 100,
-            height: 60
+            height: 60,
+            calculatedBMI: 0
         }
         this.handleWeightChange=this.handleWeightChange.bind(this);
         this.handleHeightChange=this.handleHeightChange.bind(this);
@@ -50,7 +51,7 @@ combineClearWeight(state){
                Height in inches<input onChange={this.handleHeightChange}/>
                <button id="addHeight" onClick={()=>this.combineAddClear(this.state.userInput)}>Take Height</button>
                <br/>
-               <h4>Your BMI is : {calculatedBMI}</h4>
+               <h4>Your BMI is : {this.state.calculatedBMI}</h4>
             </div>
         )
     }
